1d3ecc68873e90cdff2129ccbf99e094b5291eb2
Ksenia Shepherd <111@mail.ru>
2021-07-15 15:16:19 +0300

    added fixes

--- src/Transfer.java
+++ src/Transfer.java
@@ -2,7 +2,7 @@ public class Transfer {
     public static void main(String[] args) {
         int currentBalance = 2_000_000_000;
         int transferAmount = 500_000_000;
-        int total = currentBalance + 500_000_000;
+        int total = currentBalance + transferAmount;
         System.out.println(total);
     }
 }
